ef5b1b9031945773178c51670668eeb6819a15cd
culbertp <culbertp>
2009-11-09 18:13:17 +0000

    fixed clock in clock out

--- src/com/efi/printsmith/service/DataService.java
+++ src/com/efi/printsmith/service/DataService.java
@@ -289,7 +289,7 @@ public class DataService {
     		DateFormat dateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd");
     		java.util.Date date = new java.util.Date();
     		
-    		String queryString = "from "+className+" where to_char(breaktime,'YYYY-MM-DD') = '"+dateFormat.format(date).toString() + "' and employee = :employee and endDateTime = ''";
+    		String queryString = "from "+className+" where to_char(breaktime,'YYYY-MM-DD') = '"+dateFormat.format(date).toString() + "' and employee = :employee";
 			Query query = em.createQuery(queryString);
 			query.setParameter("employee", employee);
     		List<TimeCard>  timecardlist = query.getResultList();   
@@ -301,7 +301,28 @@ public class DataService {
    		}        
     	return null;            
    	} 
-    
+
+	public List<TimeCard> getByTimeCardByEmployee(String className, Employee employee) throws Exception {      
+    	try {            
+    		log.debug("** getByEmployee Id called...");                
+    		EntityManager em = entityManagerFactory.createEntityManager();  
+    		
+    		DateFormat dateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd");
+    		java.util.Date date = new java.util.Date();
+    		
+    		String queryString = "from "+className+" where to_char(created,'YYYY-MM-DD') = '"+dateFormat.format(date).toString() + "' and employee = :employee";
+			Query query = em.createQuery(queryString);
+			query.setParameter("employee", employee);
+    		List<TimeCard>  timecardlist = query.getResultList();   
+    		if (timecardlist != null)
+    		 	log.debug("** Found " + timecardlist.size() + "records:");
+    		return timecardlist;
+    	} catch (Exception e) { 
+    		log.error(e);       
+   		}        
+    	return null;            
+   	} 
+
     
     public List<SecuritySetup> getByAccessGroup(String className, AccessGroup accessGroup) throws Exception {      
     	try {            
