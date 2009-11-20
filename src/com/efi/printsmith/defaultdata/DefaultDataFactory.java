a3419879083076a1c76e3f0d42fcd3e648e2ea1c
culbertp <culbertp>
2009-11-20 20:28:28 +0000

    default data fixed... will not duplicate data

--- src/com/efi/printsmith/defaultdata/DefaultDataFactory.java
+++ src/com/efi/printsmith/defaultdata/DefaultDataFactory.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import com.efi.printsmith.service.DataService;
@@ -31,6 +32,9 @@ public class DefaultDataFactory {
 		ProcessCountryFile();
 		ProcessUsers();
 		ProcessCreditCards();
+		ProcessComLinkType();
+		ProcessShippingMethod();
+		ProcessProductionLocations();
 	}
 
 	private void ProcessStateFile(){
@@ -42,6 +46,10 @@ public class DefaultDataFactory {
 	}
 	
 	private void ProcessAccessGroup(){
+		List<?> itemList = (List<?>) dataservice.getAll("AccessGroup");
+
+		if (itemList.size() > 0) return ;
+
 		AccessGroup accessgroup = new AccessGroup();
 		accessgroup.setName("System");
 		try {
@@ -52,6 +60,10 @@ public class DefaultDataFactory {
 	}
 	
 	private void ProcessEmployee(){
+		List<?> itemList = (List<?>) dataservice.getAll("Employee");
+
+		if (itemList.size() > 0) return ;
+
 		Employee employee = new Employee();
 		employee.setFirstName("Efi");
 		employee.setLastName("Printsmith");
@@ -63,6 +75,10 @@ public class DefaultDataFactory {
 	}
 
 	private void ProcessUsers(){
+		List<?> itemList = (List<?>) dataservice.getAll("Users");
+
+		if (itemList.size() > 0) return ;
+
 		Users users = new Users();
 		users.setName("admin");
 		users.setPassword("admin");
@@ -82,6 +98,11 @@ public class DefaultDataFactory {
 	}
 	
 	private void ProcessCreditCards(){
+		
+		List<?> creditcardList = (List<?>) dataservice.getAll("PreferencesCreditCard");
+
+		if (creditcardList.size() > 0) return ;
+
 		PreferencesCreditCard creditcard = new PreferencesCreditCard();
 		creditcard.setAbbreviation("AMEX");
 		creditcard.setCardType("American Express");
@@ -123,10 +144,165 @@ public class DefaultDataFactory {
 		}
 	}
 
+	private void ProcessComLinkType(){
+		List<?> comLinkTypeList = (List<?>) dataservice.getAll("ComLinkType");
+
+		if (comLinkTypeList.size() > 0) return ;
+
+		ComLinkType comLinkType = new ComLinkType();
+		comLinkType.setType("Phone");
+		try {
+			dataservice.addUpdate(comLinkType);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		comLinkType = new ComLinkType();
+		comLinkType.setType("Cell");
+		try {
+			dataservice.addUpdate(comLinkType);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		comLinkType = new ComLinkType();
+		comLinkType.setType("Fax");
+		try {
+			dataservice.addUpdate(comLinkType);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		comLinkType = new ComLinkType();
+		comLinkType.setType("E-Mail");
+		try {
+			dataservice.addUpdate(comLinkType);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		comLinkType = new ComLinkType();
+		comLinkType.setType("IM");
+		try {
+			dataservice.addUpdate(comLinkType);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		comLinkType = new ComLinkType();
+		comLinkType.setType("FaceBook");
+		try {
+			dataservice.addUpdate(comLinkType);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		comLinkType = new ComLinkType();
+		comLinkType.setType("Twitter");
+		try {
+			dataservice.addUpdate(comLinkType);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		comLinkType = new ComLinkType();
+		comLinkType.setType("LinkedIn");
+		try {
+			dataservice.addUpdate(comLinkType);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		comLinkType = new ComLinkType();
+		comLinkType.setType("Web Site");
+		try {
+			dataservice.addUpdate(comLinkType);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+	}
+	
+	private void ProcessShippingMethod(){
+		List<?> shippingMethodList = (List<?>) dataservice.getAll("ShippingMethod");
+
+		if (shippingMethodList.size() > 0) return ;
+
+		ShippingMethod shippingMethod = new ShippingMethod();
+		shippingMethod.setName("Will Call");
+		try {
+			dataservice.addUpdate(shippingMethod);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		shippingMethod = new ShippingMethod();
+		shippingMethod.setName("Call");
+		try {
+			dataservice.addUpdate(shippingMethod);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		shippingMethod = new ShippingMethod();
+		shippingMethod.setName("Deliver");
+		try {
+			dataservice.addUpdate(shippingMethod);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		shippingMethod = new ShippingMethod();
+		shippingMethod.setName("Ground");
+		try {
+			dataservice.addUpdate(shippingMethod);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		shippingMethod = new ShippingMethod();
+		shippingMethod.setName("FexEx");
+		try {
+			dataservice.addUpdate(shippingMethod);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		shippingMethod = new ShippingMethod();
+		shippingMethod.setName("UPS");
+		try {
+			dataservice.addUpdate(shippingMethod);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		shippingMethod = new ShippingMethod();
+		shippingMethod.setName("UPS Red");
+		try {
+			dataservice.addUpdate(shippingMethod);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		shippingMethod = new ShippingMethod();
+		shippingMethod.setName("UPS Blue");
+		try {
+			dataservice.addUpdate(shippingMethod);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		shippingMethod = new ShippingMethod();
+		shippingMethod.setName("USPS");
+		try {
+			dataservice.addUpdate(shippingMethod);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+		shippingMethod = new ShippingMethod();
+		shippingMethod.setName("International");
+		try {
+			dataservice.addUpdate(shippingMethod);
+		} catch (Exception e) {
+			log.debug("** Exception: " + e.getMessage());
+		}
+
+	}
+
+	private void ProcessProductionLocations(){
+		try {
+			LoadProductionLocationsData(new String[]{currentPath});
+		} catch (IOException e) {
+			log.debug("** Exception: States file Load failed.");
+		}
+	}
+	
 	private void LoadStatesData(String[] args) throws IOException
 	{
 		if (args.length == 0) args = new String[]{".."};
-		
 		String path = new File(args[0]).getParent();
 		File pathName = new File(path);
 		String[] fileNames = pathName.list();
@@ -146,7 +322,9 @@ public class DefaultDataFactory {
 	}
 	
 	private int doStatesFile(File file) throws java.io.IOException{
-		
+
+		List<?> stateList= (List<?>) dataservice.getAll("State");
+
 		FileInputStream f = new FileInputStream(file);
 		InputStreamReader ip = new InputStreamReader(f);
 	    java.io.BufferedReader br = new java.io.BufferedReader(ip);
@@ -155,14 +333,35 @@ public class DefaultDataFactory {
 	    while ((line = br.readLine()) != null){
 	    	if (line.length() > 0)
 	    	{
-	    		State state = new State();
-	    		state.setName(line.trim());
-				try {
-					dataservice.addUpdate(state);
-				} catch (Exception e) {
-					log.debug("** Exception: " + e.getMessage());
-					break;
-				}
+	    		if (stateList.size() > 0){
+	    			boolean found = false;
+	    			for (int i = 0; i < stateList.size(); i++)
+    	    		{
+    	    			if (((State)stateList.get(i)).getName().trim().equals(line.trim()) == true){
+    	    				found = true;
+    	    				break;
+    	    			}
+	    			}
+	    			if (found != true){
+	    				State state = new State();
+	    	    		state.setName(line.trim());
+	    				try {
+	    					dataservice.addUpdate(state);
+	    				} catch (Exception e) {
+	    					log.debug("** Exception: " + e.getMessage());
+	    					break;
+	    				}
+	    			}
+	    		}else{
+					State state = new State();
+		    		state.setName(line.trim());
+					try {
+						dataservice.addUpdate(state);
+					} catch (Exception e) {
+						log.debug("** Exception: " + e.getMessage());
+						break;
+					}
+	    		}
 	    	}
 	    }
 	    return rv;
@@ -192,6 +391,8 @@ public class DefaultDataFactory {
 	
 	private int doCountryFile(File file) throws java.io.IOException{
 		
+		List<?> countryList= (List<?>) dataservice.getAll("Country");
+
 		FileInputStream f = new FileInputStream(file);
 		InputStreamReader ip = new InputStreamReader(f);
 	    java.io.BufferedReader br = new java.io.BufferedReader(ip);
@@ -200,14 +401,102 @@ public class DefaultDataFactory {
 	    while ((line = br.readLine()) != null){
 	    	if (line.length() > 0)
 	    	{
-	    		Country country = new Country();
-	    		country.setName(line.trim());
-				try {
-					dataservice.addUpdate(country);
-				} catch (Exception e) {
-					log.debug("** Exception: " + e.getMessage());
-					break;
+	    		if (countryList.size() > 0){
+	    			boolean found = false;
+	    			for (int i = 0; i < countryList.size(); i++)
+    	    		{
+    	    			if (((Country)countryList.get(i)).getName().trim().equals(line.trim()) == true){
+    	    				found = true;
+    	    				break;
+    	    			}
+	    			}
+	    			if (found != true){
+	    				Country country = new Country();
+	    				country.setName(line.trim());
+	    				try {
+	    					dataservice.addUpdate(country);
+	    				} catch (Exception e) {
+	    					log.debug("** Exception: " + e.getMessage());
+	    					break;
+	    				}
+	    			}
+	    		} else{
+		    		Country country = new Country();
+		    		country.setName(line.trim());
+					try {
+						dataservice.addUpdate(country);
+					} catch (Exception e) {
+						log.debug("** Exception: " + e.getMessage());
+						break;
+					}
+	    		}
+	    	}
+	    }
+	    return rv;
+	}
+
+	private void LoadProductionLocationsData(String[] args) throws IOException
+	{
+		if (args.length == 0) args = new String[]{".."};
+		String path = new File(args[0]).getParent();
+		File pathName = new File(path);
+		String[] fileNames = pathName.list();
+		for (int i = 0; i <fileNames.length; i++)
+		{
+			if (fileNames[i].endsWith(".txt") == true && fileNames[i].toLowerCase().startsWith("state")==true)
+			{
+				File f = new File(pathName.getPath(),fileNames[i]);
+				int result = doProductionLocationsFile(f);
+				if (result < 0)
+				{
+					log.debug("** Exception: ProductionLocations file Load failed.");
 				}
+				break;
+			}			
+		}
+	}
+
+	private int doProductionLocationsFile(File file) throws java.io.IOException{
+		
+		List<?> productionLocationsList= (List<?>) dataservice.getAll("ProductionLocations");
+
+		FileInputStream f = new FileInputStream(file);
+		InputStreamReader ip = new InputStreamReader(f);
+	    java.io.BufferedReader br = new java.io.BufferedReader(ip);
+	    String line = null;
+	    int rv = -1;
+	    while ((line = br.readLine()) != null){
+	    	if (line.length() > 0)
+	    	{
+	    		if (productionLocationsList.size() > 0){
+	    			boolean found = false;
+	    			for (int i = 0; i < productionLocationsList.size(); i++)
+    	    		{
+    	    			if (((ProductionLocations)productionLocationsList.get(i)).getName().trim().equals(line.trim()) == true){
+    	    				found = true;
+    	    				break;
+    	    			}
+	    			}
+	    			if (found != true){
+	    				ProductionLocations productionLocations = new ProductionLocations();
+	    				productionLocations.setName(line.trim());
+	    				try {
+	    					dataservice.addUpdate(productionLocations);
+	    				} catch (Exception e) {
+	    					log.debug("** Exception: " + e.getMessage());
+	    					break;
+	    				}
+	    			}
+	    		} else{
+    				ProductionLocations productionLocations = new ProductionLocations();
+    				productionLocations.setName(line.trim());
+    				try {
+    					dataservice.addUpdate(productionLocations);
+    				} catch (Exception e) {
+    					log.debug("** Exception: " + e.getMessage());
+    					break;
+    				}
+	    		}
 	    	}
 	    }
 	    return rv;
