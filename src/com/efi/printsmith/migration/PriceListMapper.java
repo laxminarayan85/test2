33ebc972d7a961d872d2c385a59689267ab20c62
vassallm <vassallm>
2010-02-22 18:05:58 +0000

    price list import complete

--- src/com/efi/printsmith/migration/PriceListMapper.java
+++ src/com/efi/printsmith/migration/PriceListMapper.java
@@ -5,98 +5,216 @@ import java.util.ArrayList;
 import com.efi.printsmith.data.Invoice;
 import com.efi.printsmith.data.ModelBase;
 import com.efi.printsmith.data.PriceList;
+import com.efi.printsmith.data.PriceListBase;
+import com.efi.printsmith.data.PriceListElement;
+import com.efi.printsmith.service.DataService;
 
 public class PriceListMapper extends ImportMapper {
-	public ModelBase importTokens(String[] fieldTokens, String[] importTokens) throws Exception {
+	@SuppressWarnings("null")
+	public ModelBase importTokens(String[] fieldTokens, String[] importTokens)
+			throws Exception {
 		PriceList priceList = new PriceList();
-		
-		for (int i=0; i < fieldTokens.length; i++) {
-			String currentImportToken = importTokens[i];
-			String currentFieldToken = fieldTokens[i];
-			                                       
-			if ("recno".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("rtype".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("title".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[1]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[2]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[3]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[4]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[5]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[6]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[7]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[8]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[9]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[10]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[11]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[12]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[13]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[14]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("quantity[15]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[1]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[2]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[3]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[4]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[5]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[6]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[7]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[8]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[9]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[10]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[11]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[12]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[13]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[14]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("amount[15]".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("doAddl".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("is Pct".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("is dollar".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("interpolate".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("is rate".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("isDiscount".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("special".equals(currentFieldToken)) {
-				/* TODO */
-			} else if ("rstatus".equals(currentFieldToken)) {
-				/* TODO */
+		for (int x = 1; x <= 15; x++) {
+			PriceListElement priceListElement = null;
+			for (int i = 0; i < fieldTokens.length; i++) {
+				String currentImportToken = importTokens[i];
+				String currentFieldToken = fieldTokens[i];
+				if ("recno".equals(currentFieldToken)) {
+					/* TODO */
+				} else if ("rtype".equals(currentFieldToken)) {
+					/* TODO */
+				} else if ("title".equals(currentFieldToken) && x == 1) {
+					priceList.setName(currentImportToken);
+				} else if ("quantity[1]".equals(currentFieldToken) && x == 1) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[2]".equals(currentFieldToken) && x == 2) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[3]".equals(currentFieldToken) && x == 3) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[4]".equals(currentFieldToken) && x == 4) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[5]".equals(currentFieldToken) && x == 5) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[6]".equals(currentFieldToken) && x == 6) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[7]".equals(currentFieldToken) && x == 7) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[8]".equals(currentFieldToken) && x == 8) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[9]".equals(currentFieldToken) && x == 9) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[10]".equals(currentFieldToken) && x == 10) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[11]".equals(currentFieldToken) && x == 11) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[12]".equals(currentFieldToken) && x == 12) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[13]".equals(currentFieldToken) && x == 13) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[14]".equals(currentFieldToken) && x == 14) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("quantity[15]".equals(currentFieldToken) && x == 15) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement = new PriceListElement();
+						priceListElement.setQuantity(Utilities
+								.tokenToLong(currentImportToken));
+					}
+				} else if ("amount[1]".equals(currentFieldToken) && x == 1) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[2]".equals(currentFieldToken) && x == 2) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[3]".equals(currentFieldToken) && x == 3) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[4]".equals(currentFieldToken) && x == 4) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[5]".equals(currentFieldToken) && x == 5) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[6]".equals(currentFieldToken) && x == 6) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[7]".equals(currentFieldToken) && x == 7) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[8]".equals(currentFieldToken) && x == 8) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[9]".equals(currentFieldToken) && x == 9) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[10]".equals(currentFieldToken) && x == 10) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[11]".equals(currentFieldToken) && x == 11) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[12]".equals(currentFieldToken) && x == 12) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[13]".equals(currentFieldToken) && x == 13) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[14]".equals(currentFieldToken) && x == 14) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("amount[15]".equals(currentFieldToken) && x == 15) {
+					if (currentImportToken.equals("") == false) {
+						priceListElement.setAmount(Utilities
+								.tokenToDouble(currentImportToken));
+					}
+				} else if ("doAddl".equals(currentFieldToken)) {
+					/* TODO */
+				} else if ("is Pct".equals(currentFieldToken) && x == 1) {
+					priceList.setIsPercentage(Utilities
+							.tokenToBooleanValue(currentImportToken));
+				} else if ("is dollar".equals(currentFieldToken) && x == 1) {
+					priceList.setIsDollar(Utilities
+							.tokenToBooleanValue(currentImportToken));
+				} else if ("interpolate".equals(currentFieldToken) && x == 1) {
+					priceList.setInterpolate(Utilities
+							.tokenToBooleanValue(currentImportToken));
+				} else if ("is rate".equals(currentFieldToken) && x == 1) {
+					priceList.setIsRate(Utilities
+							.tokenToBooleanValue(currentImportToken));
+				} else if ("isDiscount".equals(currentFieldToken) && x == 1) {
+					priceList.setIsDiscount(Utilities
+							.tokenToBooleanValue(currentImportToken));
+				} else if ("special".equals(currentFieldToken)) {
+					/* TODO */
+				} else if ("rstatus".equals(currentFieldToken)) {
+					/* TODO */
+				}
 			}
+			if (priceListElement != null)
+				priceList.addElements(priceListElement);
 		}
 		return priceList;
 	}
