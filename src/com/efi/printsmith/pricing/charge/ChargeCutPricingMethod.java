aec8dfb1c0c4fd43bbaa63e4b74ac1f29b2d29dd
knabelb <knabelb>
2010-03-24 18:19:32 +0000

    Update charge method.

--- src/com/efi/printsmith/pricing/charge/ChargeCutPricingMethod.java
+++ src/com/efi/printsmith/pricing/charge/ChargeCutPricingMethod.java
@@ -1,9 +1,26 @@
 package com.efi.printsmith.pricing.charge;
 
 import com.efi.printsmith.data.Charge;
+import com.efi.printsmith.data.ChargeDefinition;
+import com.efi.printsmith.data.InkCharge;
+import com.efi.printsmith.data.JobBase;
 
 public class ChargeCutPricingMethod extends ChargePricingMethod {
 	public Charge priceCharge(Charge charge) {
+		InkCharge localCharge = (InkCharge) charge;
+		ChargeDefinition chargeDefinition = localCharge.getChargeDefinition();
+		JobBase job = localCharge.getParentJob();
+		int jobQty = 1;
+		double price = 0.0;
+		
+		if (chargeDefinition == null) return localCharge;
+		// TODO: Handle multi-part jobs per PS
+		
+		ChargeJobAwarePricingMethod jobAwareMethod = new ChargeJobAwarePricingMethod();
+		
+		// TODO: Handle cuts are pre-press
+		charge = jobAwareMethod.priceCharge(charge);
+		
 		return charge;
 	}
 
