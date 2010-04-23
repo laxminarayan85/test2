eb99c1d72ed2f94e055e361d63503078b7883dd1
vassallm <vassallm>
2010-04-23 14:46:54 +0000

    added StampSchedule

---
+++ flex_src/com/efi/printsmith/data/StampSchedule.as
@@ -0,0 +1,18 @@
+package com.efi.printsmith.data
+{
+	import mx.collections.ArrayCollection; 
+	import com.efi.printsmith.data.*;
+	
+	[RemoteClass(alias="com.efi.printsmith.data.StampSchedule")]
+	[Managed]
+	
+	public class StampSchedule extends Matrix
+	{
+
+		public function StampSchedule() {
+			remoteObjectName = "com.efi.printsmith.data.StampSchedule";
+		}
+				
+	
+	}
+}
---
+++ src/com/efi/printsmith/data/StampSchedule.java
@@ -0,0 +1,52 @@
+
+package com.efi.printsmith.data;
+
+import java.util.Date;
+import java.util.List;
+
+import com.efi.printsmith.data.*;
+import com.efi.printsmith.data.enums.*;
+import com.efi.printsmith.exceptions.*;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CollectionOfElements;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.hibernate.annotations.IndexColumn;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+import org.hibernate.annotations.Type;
+
+/**
+ * @generated
+ */
+
+ 
+/**
+ * @generated
+ */	
+@NamedQueries({
+	@NamedQuery(name = "StampSchedule.findall", query = "from StampSchedule"),
+	@NamedQuery(name = "StampSchedule.byId", query = "select a from StampSchedule a where a.id= :id")
+})
+
+
+
+/**
+ * @generated
+ */	
+@Entity
+@Table(name = "stampschedule")
+public class StampSchedule extends Matrix {
+
+	/**
+	 * @generated
+	 */
+	public StampSchedule() {
+		this.created = new Date();
+		this.modified = new Date();
+	}
+
+}
