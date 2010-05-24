1cba0d1e3b8a770dfe8a50c801fa94a4d3d0322a
knabelb <knabelb>
2010-05-24 16:10:43 +0000

    Provide example for returning several results from service call.

--- src/com/efi/printsmith/service/PaperCalculatorService.java
+++ src/com/efi/printsmith/service/PaperCalculatorService.java
@@ -1,7 +1,9 @@
 package com.efi.printsmith.service;
 
 import java.security.InvalidParameterException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.EntityManagerFactory;
 
@@ -24,27 +26,33 @@ public class PaperCalculatorService extends SnowmassHibernateService {
 	
 	protected static EntityManagerFactory entityManagerFactory = null;
 
-	public Integer CalcOut(JobBase job) throws Exception {
+	public List CalcOut(JobBase job) throws Exception {
 		int out= 0;
+		ArrayList results = new ArrayList();
 		
 		if (job == null) {
 			throw new InvalidParameterException("Null job passed to CalcOut");
 		}
 		log.info("CalcOut called with job: " + job.getId());
 		
-		/* Just for illustration: */
-		if (job.getPaperCal() != null) {
-			if (job.getPaperCal().getFolioEdge().equals(Constants.PAPER_CALCULATOR_FOLIO_LEFT)) {
-				out = 1;
-			} else if (job.getPaperCal().getFolioEdge().equals(Constants.PAPER_CALCULATOR_FOLIO_TOP)) {
-				out = 2;
-			} else {
-				throw new InvalidParameterException("Invalid value in Paper Calculator's FolioEdge");
-			}
-		}
+//		/* Just for illustration: */
+//		if (job.getPaperCal() != null) {
+//			if (job.getPaperCal().getFolioEdge().equals(Constants.PAPER_CALCULATOR_FOLIO_LEFT)) {
+//				out = 1;
+//			} else if (job.getPaperCal().getFolioEdge().equals(Constants.PAPER_CALCULATOR_FOLIO_TOP)) {
+//				out = 2;
+//			} else {
+//				throw new InvalidParameterException("Invalid value in Paper Calculator's FolioEdge");
+//			}
+//		}
 		
 		log.info("CalcOut returned: " + out);
-		return out;
+		
+		results.add(out);
+		results.add("Value Two");
+		results.add(new Double(5.0));
+		
+		return results;
 	}
 	
 	
