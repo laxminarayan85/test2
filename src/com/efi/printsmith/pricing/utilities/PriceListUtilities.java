25baa9022dedf4217b7a6e8e8f819db20808862b
vassallm <vassallm>
2010-07-14 16:33:25 +0000

    3496

--- src/com/efi/printsmith/pricing/utilities/PriceListUtilities.java
+++ src/com/efi/printsmith/pricing/utilities/PriceListUtilities.java
@@ -13,12 +13,13 @@ import com.efi.printsmith.data.Dimension;
 import com.efi.printsmith.migration.Utilities;
 
 public class PriceListUtilities {
-	static public double calculatePriceListPrice(long qty, PriceList priceList, double chargePrice, Job job) {
+	static public double calculatePriceListPrice(long qty, PriceList priceList,
+			double chargePrice, Job job) {
 		double retVal = 0.0;
 		double rate = 0.0;
-		
+
 		rate = PriceListUtilities.lookupPrice(priceList, qty);
-		
+
 		if (priceList.getIsRate() && priceList.getIsPercentage()) {
 			retVal = chargePrice * rate;
 		} else if (priceList.getIsRate()) {
@@ -27,52 +28,60 @@ public class PriceListUtilities {
 			retVal = chargePrice * rate;
 		} else if (priceList.getIsDiscount()) {
 			if (job != null) {
-				retVal = job.getPricingRecord().getTotalPrice().doubleValue() * rate;
+				retVal = job.getPricingRecord().getTotalPrice().doubleValue()
+						* rate;
 			}
 		} else {
 			retVal = rate;
 		}
 		return retVal;
 	}
-	
-	static public double lookupPaperPrice(PriceListBase priceList, long copies, long colors, long side) {
+
+	static public double lookupPaperPrice(PriceListBase priceList, long copies,
+			long colors, long side) {
 		double retVal = 0.0;
-		
-		if (priceList == null) return retVal;
-		
+
+		if (priceList == null)
+			return retVal;
+
 		List<PriceListElement> elements = priceList.getElements();
 
-		if (elements.size() <= 0) return retVal;
-		
+		if (elements.size() <= 0)
+			return retVal;
+
 		for (int i = 0; i < elements.size(); i++) {
-			if (elements.get(i).getQuantity() >= copies  && elements.get(i).getColor() == colors && elements.get(i).getSide() == side) {
+			if (elements.get(i).getQuantity() >= copies
+					&& elements.get(i).getColor() == colors
+					&& elements.get(i).getSide() == side) {
 				retVal = elements.get(i).getAmount().doubleValue();
 				break;
 			}
 		}
-		
+
 		return retVal;
 	}
-	
+
 	static public double lookupPrice(PriceListBase priceList, long qty) {
 		double retVal = 0.0;
 		long lookupQty = qty;
 		int i = 0;
 		int base = 0;
 		int last = 0;
-		
+
 		boolean doAdditional = priceList.getLastItemIsPriceAdditionalQty();
-		
-		if (priceList == null) return retVal;
-		
+
+		if (priceList == null)
+			return retVal;
+
 		if (priceList.getIsRate()) {
 			doAdditional = false;
 		}
 
 		List<PriceListElement> elements = priceList.getElements();
 
-		if (elements.size() <= 0) return retVal;
-		
+		if (elements.size() <= 0)
+			return retVal;
+
 		if (lookupQty <= elements.get(0).getQuantity()) {
 			retVal = elements.get(0).getAmount().doubleValue();
 		} else {
@@ -82,14 +91,14 @@ public class PriceListUtilities {
 				// Find the actual last element
 				if (elements.get(i).getQuantity() == 0) {
 					found = true;
-					lastElementIndex = i-1;
+					lastElementIndex = i - 1;
 					break;
 				}
 			}
-			
+
 			if (found == false)
-				lastElementIndex = i-1;
-			
+				lastElementIndex = i - 1;
+
 			for (i = lastElementIndex; i >= 0; i--) {
 				// Search backwards for matching element
 				if (elements.get(i).getQuantity() <= lookupQty) {
@@ -97,7 +106,7 @@ public class PriceListUtilities {
 					break;
 				}
 			}
-			
+
 			if (base < lastElementIndex) {
 				if (elements.get(base).getQuantity() == lookupQty) {
 					// Found exact item
@@ -108,43 +117,59 @@ public class PriceListUtilities {
 			} else {
 				last = base;
 			}
-			
-			if (doAdditional && elements.get(elements.size()-1).getQuantity()==0) {
+
+			if (doAdditional
+					&& elements.get(elements.size() - 1).getQuantity() == 0) {
 				doAdditional = false; // There's no additional value
 			} else if (doAdditional) {
 				if (elements.get(last).getQuantity() >= lookupQty) {
 					doAdditional = false;
 				}
 			}
-			
+
 			if (doAdditional) {
 				retVal = elements.get(base).getAmount().doubleValue();
 				lookupQty = lookupQty - elements.get(base).getQuantity();
-				
+
 				if (priceList.getIsRate()) {
 					// Rates treat last element as the actual rate
-					retVal = elements.get(elements.size()-1).getQuantity();
+					retVal = elements.get(elements.size() - 1).getQuantity();
 				} else {
 					qty = lookupQty;
-					
+
 					if (priceList.getInterpolate()) {
-						retVal += (elements.get(elements.size()).getAmount().doubleValue() / elements.get(elements.size()).getQuantity()) * qty;
+						retVal += (elements.get(elements.size()).getAmount()
+								.doubleValue() / elements.get(elements.size())
+								.getQuantity())
+								* qty;
 					} else {
-						if ((lookupQty%elements.get(elements.size()).getQuantity()) != 0) {
-							lookupQty = lookupQty + (elements.get(elements.size()).getQuantity() - (lookupQty&elements.get(elements.size()).getQuantity()));
+						if ((lookupQty % elements.get(elements.size())
+								.getQuantity()) != 0) {
+							lookupQty = lookupQty
+									+ (elements.get(elements.size())
+											.getQuantity() - (lookupQty & elements
+											.get(elements.size()).getQuantity()));
 						}
-						retVal = retVal + ((lookupQty / elements.get(elements.size()).getQuantity()) * elements.get(elements.size()).getAmount().doubleValue());
+						retVal = retVal
+								+ ((lookupQty / elements.get(elements.size())
+										.getQuantity()) * elements.get(
+										elements.size()).getAmount()
+										.doubleValue());
 					}
 				}
 			} else if (priceList.getInterpolate()) {
 				retVal = elements.get(base).getAmount().doubleValue();
 				qty = lookupQty - elements.get(base).getQuantity();
-				
-				if (qty!= 0) {
-					long qtyRange = elements.get(base).getQuantity() - elements.get(last).getQuantity();
+
+				if (qty != 0) {
+					long qtyRange = elements.get(base).getQuantity()
+							- elements.get(last).getQuantity();
 					long lookupRange = qty - elements.get(last).getQuantity();
-					double priceRange = elements.get(last).getAmount().doubleValue() - retVal;
-					retVal = ((lookupRange * priceRange) / qtyRange) + elements.get(last).getAmount().doubleValue();
+					double priceRange = elements.get(last).getAmount()
+							.doubleValue()
+							- retVal;
+					retVal = ((lookupRange * priceRange) / qtyRange)
+							+ elements.get(last).getAmount().doubleValue();
 				}
 			} else {
 				retVal = elements.get(last).getAmount().doubleValue();
@@ -153,35 +178,38 @@ public class PriceListUtilities {
 		return retVal;
 	}
 
-	static public double getTimePerSheetFromSpeedTable(SpeedTable speedTable, long qty) {
+	static public double getTimePerSheetFromSpeedTable(SpeedTable speedTable,
+			long qty) {
 		double retVal = 0.0;
-		
+
 		if (speedTable != null) {
-			List <PriceListElement> speedTableElements = speedTable.getElements();
+			List<PriceListElement> speedTableElements = speedTable
+					.getElements();
 			PriceListElement elementToUse = null;
-			if(speedTableElements != null && speedTableElements.size() > 0) {
+			if (speedTableElements != null && speedTableElements.size() > 0) {
 				elementToUse = speedTableElements.get(0);
-				for (int i=1; i < speedTableElements.size(); i++) {
+				for (int i = 1; i < speedTableElements.size(); i++) {
 					PriceListElement curElement = speedTableElements.get(i);
 					if (curElement.getQuantity() > qty) {
-						elementToUse = speedTableElements.get(i-1);
+						elementToUse = speedTableElements.get(i - 1);
 						break;
 					}
 				}
 				if (elementToUse.getQuantity() > 0) {
-					retVal = 3600/elementToUse.getAmount().doubleValue();
+					retVal = 3600 / elementToUse.getAmount().doubleValue();
 				}
 			}
 		}
 		return retVal;
 	}
-	
+
 	static public double getSpeedFromSpeedTable(SpeedTable speedTable, long qty) {
 		double retVal = 0;
 		
 		if (speedTable != null) {
 			List <PriceListElement> speedTableElements = speedTable.getElements();
 			PriceListElement elementToUse = null;
+			PriceListElement lastElement = null;
 			if(speedTableElements != null && speedTableElements.size() > 0) {
 				elementToUse = speedTableElements.get(0);
 				for (int i=1; i < speedTableElements.size(); i++) {
@@ -190,19 +218,32 @@ public class PriceListUtilities {
 						elementToUse = speedTableElements.get(i-1);
 						break;
 					}
+					lastElement = elementToUse;
 				}
 				if (elementToUse.getQuantity() > 0) {
 					retVal = elementToUse.getAmount().doubleValue();
 				}
+				 if (speedTable.getInterpolate()) {
+					qty = qty - elementToUse.getQuantity();
+					
+					if (qty!= 0) {
+						long qtyRange = elementToUse.getQuantity() - lastElement.getQuantity();
+						long lookupRange = qty - lastElement.getQuantity();
+						double priceRange = lastElement.getAmount().doubleValue() - retVal;
+						retVal = ((lookupRange * priceRange) / qtyRange) + lastElement.getAmount().doubleValue();
+					}
+				 }
 			}
 		}
 		return retVal;
 	}
-	
-	static public long getAreaFromSizeString(Dimension size) throws NumberFormatException, ParseException {
-		// TODO: Need to be much more flexible in string handling here (A4, B4, different lengths of dimensions)
+
+	static public long getAreaFromSizeString(Dimension size)
+			throws NumberFormatException, ParseException {
+		// TODO: Need to be much more flexible in string handling here (A4, B4,
+		// different lengths of dimensions)
 		long retVal = 0;
-		
+
 		int i = size.getName().indexOf("x");
 		if (i > -1) {
 			String lengthString = size.getName().substring(0, i - 1);
@@ -211,31 +252,33 @@ public class PriceListUtilities {
 			float width = Utilities.tokenToFloat(widthString);
 			retVal = (long) (length * width);
 		}
-		
+
 		return retVal;
 	}
-	
-	static public float getLengthFromSizeString(Dimension size) throws NumberFormatException, ParseException {
+
+	static public float getLengthFromSizeString(Dimension size)
+			throws NumberFormatException, ParseException {
 		// TODO: Need to be much more flexible in string handling here
 		String lengthString = "0";
-		
+
 		int i = size.getName().indexOf("x");
 		if (i > -1) {
 			lengthString = size.getName().substring(0, i - 1);
 		}
-		
+
 		return Float.parseFloat(lengthString);
 	}
-	
-	static public float getWidthFromSizeString(Dimension size) throws NumberFormatException, ParseException {
+
+	static public float getWidthFromSizeString(Dimension size)
+			throws NumberFormatException, ParseException {
 		// TODO: Need to be much more flexible in string handling here
 		String widthString = "0";
-		
+
 		int i = size.getName().indexOf("x");
 		if (i > -1) {
 			widthString = size.getName().substring(i + 2);
 		}
-		
+
 		return Float.parseFloat(widthString);
 	}
 }
