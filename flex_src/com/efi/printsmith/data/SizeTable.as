bffbc9f11a97172427657d5704e586be621f13fd
madhub <madhub>
2010-07-23 09:41:29 +0000

    Fix for Issue (Bug Id - 3580)

--- flex_src/com/efi/printsmith/data/SizeTable.as
+++ flex_src/com/efi/printsmith/data/SizeTable.as
@@ -13,7 +13,7 @@ package com.efi.printsmith.data
 			remoteObjectName = "com.efi.printsmith.data.SizeTable";
 		}
 				
-		public var name:int = 0;
+		public var name:String = "";
 	
 	}
 }
--- src/com/efi/printsmith/data/SizeTable.java
+++ src/com/efi/printsmith/data/SizeTable.java
@@ -61,12 +61,12 @@ public class SizeTable extends ModelBase {
 	 * @generated
 	 */	
  	@Basic
-	private Integer name;
+	private String name;
 	
 	/**
 	 * @generated
  	 */
-	public Integer getName(){
+	public String getName(){
     return name; 
   }
 
@@ -74,7 +74,7 @@ public class SizeTable extends ModelBase {
 	/**
 	 * @generated
 	 */	
-	public void setName(Integer newVal) {
+	public void setName(String newVal) {
     this.name = newVal;
   }
 	
@@ -94,7 +94,7 @@ public class SizeTable extends ModelBase {
 	@SuppressWarnings("unchecked")
 	@Override
 	public void setProperty(String propertyName, Object newValue) throws PropertyException {
-    if (NAME.equals(propertyName)) setName((Integer)newValue); else
+    if (NAME.equals(propertyName)) setName((String)newValue); else
     super.setProperty(propertyName, newValue);
   }
 	
@@ -105,7 +105,7 @@ public class SizeTable extends ModelBase {
 	@Override
 	public Class<?>[] getPropertyClass(String propertyName) throws UnknownPropertyException {	
     if (NAME.equals(propertyName)) 
-      return new Class<?>[] {Integer.class};		
+      return new Class<?>[] {String.class};		
     return super.getPropertyClass(propertyName);
   }
 	
