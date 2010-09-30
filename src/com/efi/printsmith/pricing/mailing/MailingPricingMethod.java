e5ee4debf995c37e5465ed87ee75fda2a2dfca66
vassallm <vassallm>
2010-09-30 18:49:31 +0000

    get markup for mailing, outside services, merchandise, ect style pricing from preferencesmarkups

--- src/com/efi/printsmith/pricing/mailing/MailingPricingMethod.java
+++ src/com/efi/printsmith/pricing/mailing/MailingPricingMethod.java
@@ -1,7 +1,12 @@
 package com.efi.printsmith.pricing.mailing;
 
+import java.util.List;
+
 import com.efi.printsmith.data.Job;
 import com.efi.printsmith.data.PricingRecord;
+import com.efi.printsmith.data.PreferencesMarkups;
+import com.efi.printsmith.service.DataService;
+import com.efi.printsmith.data.PreferencesPricingMethod;
 
 public class MailingPricingMethod {
 	public Job priceMailingJob(Job job) {
@@ -10,9 +15,37 @@ public class MailingPricingMethod {
 		
 		if (job.getQtyOrdered() > 0) {
 			job.setTotalCost(job.getQtyOrdered() * job.getUnitCost().doubleValue());
-			pricingRecord.setTotalPrice(job.getTotalCost().doubleValue() * job.getMarkup());
+			double markup = getMarkup(job.getTotalCost().doubleValue(), job.getPricingMethod());
+			job.setMarkup(markup);
+			pricingRecord.setTotalPrice(job.getTotalCost().doubleValue() * markup);
 		}
 		
 		return job;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private double getMarkup(double cost, PreferencesPricingMethod pricingMethod) {
+		double retVal = 1.0;
+		DataService dataService = new DataService();
+		List <PreferencesMarkups> markups = null;
+		if (pricingMethod.getTitle().equals("Mailing") || pricingMethod.getTitle().equals("Outside Services")) {
+			markups = (List<PreferencesMarkups>)dataService.getAll("OutsideServices");
+		} else {
+			markups = (List<PreferencesMarkups>)dataService.getAll("Merchandise");
+		}
+		
+		for (int i = 0; i < markups.size(); i ++) {
+			if (markups.get(i).getAbove()) {
+				retVal = markups.get(i).getMarkup();
+				break;
+			} else {
+				if (cost > markups.get(i).getTotalCost().doubleValue()) {
+					retVal = markups.get(i).getMarkup();
+					break;
+				}
+			}
+		}
+		
+		return retVal;
 	}
 }
